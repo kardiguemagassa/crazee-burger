8047d6f5b61d5ebd4d2feeb352535df3a5ff67f9
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-12-11 18:32:08 +0100

    ✨F01: LOGIN> Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { useState } from "react";
 
 export default function LoginPage() {
+
+  const [inputValue, setinputValue] = useState("Bob")
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour, ${inputValue} !`);
+    setinputValue("");
+  }
+
+  const handleChange = (event) => {
+    setinputValue(event.target.value);
+  }
+
   return (
-    <div>LoginPage</div>
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+          <input 
+            value={inputValue} 
+            onChange={handleChange} 
+            type="text" 
+            placeholder='Entrez votre prénom...' 
+            required />
+          <button type="submit">Accédez à votre espace</button>
+      </form>
+    </div>
   )
 }
