21f64471415190343973fb129b6dfdd0e2b82d32
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-12-12 12:07:01 +0100

    ✨ F02: APP > install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import LoginPage from "./components/pages/login/LoginPage.jsx"  
+import { Route, Routes } from "react-router-dom"
+import OrderPage from "./components/pages/order/OrderPage.jsx"
+import ErrorPage from "./components/pages/error/ErrorPage.jsx"
 
 function App() {
-  return <LoginPage />
+
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/order" element={<OrderPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  )
 }
 
 export default App
