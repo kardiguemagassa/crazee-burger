3aa22b226ee36bd5bb69250593b01232a43bc1c7
Kardigué MAGASSA <magassakara@gmail.com>
2026-09-22 17:58:35 +0200

    F04> add and update src/components/pages/order/Main.jsx, src/components/pages/order/Navbar.jsx,src/components/pages/order/NavbarRightSide.jsx,src/components/pages/order/OrderPage.jsx, src/components/pages/order/Profile.jsx,   src/components/reusable-ui/Logo.jsx,  src/utils/window.jsx,  src/components/pages/order/Main.jsx,  src/components/pages/order/Navbar.jsx,   src/components/pages/order/NavbarRightSide.jsx,src/components/pages/order/Profile.jsx,

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,15 +1,39 @@
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom"
+import styled from "styled-components"
+import { theme } from "../../../theme"
+import Main from "./Main"
+import Navbar from "./Navbar"
 
 export default function OrderPage() {
+    // state
+    const { username } = useParams()
 
-    const {username} = useParams();
+    // comportements
 
-  return (
-    <div>
-        <h1>Bojour {username}</h1>
-        <Link to="/">
-            <button>Déconnexion</button>
-        </Link>
-    </div>
-  )
+    //affichage
+    return (
+        <OrderPageStyled>
+            <div className="container">
+                <Navbar username={username} />
+                <Main />
+            </div>
+        </OrderPageStyled>
+    )
 }
+
+const OrderPageStyled = styled.div`
+    background: ${theme.colors.primary};
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .container {
+        background: red;
+        height: 95vh;
+        width: 1400px;
+        display: flex;
+        flex-direction: column;
+        border-radius: ${theme.borderRadius.extraRound};
+    }
+`
