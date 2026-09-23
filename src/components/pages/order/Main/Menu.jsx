4e31bef39083c2cf165dc7ae85fb5d2c0f77bf11
Kardigué MAGASSA <magassakara@gmail.com>
2026-09-23 16:47:40 +0200

    add package-lock.json, package.json,src/components/pages/order/Main/Menu.jsx, src/components/pages/order/Navbar/NavbarRightSide.jsx,src/components/pages/order/Navbar/NavbarRightSideIncomplet.js, src/components/pages/order/Navbar/NavbarRightSideIncomplet.jsx, src/components/pages/order/Navbar/ToastAdmin.js -> src/components/pages/order/Navbar/ToastAdmin.jsx, src/components/reusable-ui/ToggleButton.jsx, yarn.lock

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -25,11 +25,11 @@ export default function Menu() {
 }
 
 const MenuStyled = styled.div`
-  background: ${theme.colors.background_white};
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-row-gap: 60px;
-  padding: 50px 50px 150px;
-  justify-items: center;
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+    background: ${theme.colors.background_white};
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-row-gap: 60px;
+    padding: 50px 50px 150px;
+    justify-items: center;
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 `
