efa68f7f27eb196ee23175c1a40e71598632ae63
Kardigué MAGASSA <magassakara@gmail.com>
2026-09-24 12:10:53 +0200

    F07:ADMIN > Add AdminTabs, AdminPanel, tabsConfig, reusable-ui/Tab: Update: Admin/Admin, src/index.css

--- src/components/pages/order/Main/Admin/Admin.jsx
+++ src/components/pages/order/Main/Admin/Admin.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react"
 import styled from "styled-components"
 import OrderContext from "../../../../../context/OrderContext"
-import AdminPanel from "./AdminPanel"
-import AdminTabs from "./AdminTabs"
+import AdminPanel from "./AdminPanel.jsx"
+import AdminTabs from "./AdminTabs.jsx"
 
 export default function Admin() {
   const { isCollapsed } = useContext(OrderContext)
