e7a80a8caf9bbbedabe57108de8b191dca52b059
Kardigué MAGASSA <magassakara@gmail.com>
2026-09-24 10:31:39 +0200

    F07: THEME > Update font-size and shadow

--- src/components/reusable-ui/TextInput.jsx
+++ src/components/reusable-ui/TextInput.jsx
@@ -28,7 +28,7 @@ const InputStyled = styled.div`
 
   input {
     border: none;
-    font-size: ${theme.fonts.size.P0};
+    font-size: ${theme.fonts.size.SM};
     color: ${theme.colors.dark};
     width: 100%;
     /* display: flex; */
